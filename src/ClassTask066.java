486b947af757eae030b9bb05d286ef229ee0f1ac
Tetyana <tanyarunko@gmail.com>
2021-10-11 10:14:50 -0400

    added new file

--- src/ClassTask066.java
+++ src/ClassTask066.java
@@ -19,6 +19,8 @@ public class ClassTask066 {
         list.add('#');
         list.add('&');
         list.add('%');
+       // list.add('&');
+       // list.add('%');
 
 
         removeNonAlphaNumeric(list);
